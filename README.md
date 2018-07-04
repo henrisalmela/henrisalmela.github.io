7a9ae75c42464cf6dd32ccb346a0870959515d81
henrisalmela <henri.salmela@outlook.com>
2018-07-04 20:34:32 +0300

    ..

---
+++ README.md
@@ -0,0 +1,2 @@
+# henrisalmela.github.io
+This is my personal web-portfolio project to boost my UX career, while simultaneously practicing html and css.
---
+++ js.js
@@ -0,0 +1,17 @@
+	
+
+	/*document.querySelector(".button").addEventListener("click", function() {
+		document.querySelector(".button").classList.toggle("hulk-cta");
+	}); 
+
+	document.querySelector(".exp-button").addEventListener("click", function() {
+		document.querySelector(".link-list").classList.toggle("exp-list");
+	});
+
+*/
+	document.querySelector(".container").addEventListener("click", function() {
+		document.querySelector(".expContainer").classList.toggle("show-exp-container");
+
+	});
+
+
